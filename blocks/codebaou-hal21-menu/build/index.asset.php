8ec2b8f234b1750a8ff8dbae66c6950141ea57be
CodeBAou <borisafou@gmail.com>
2024-09-30 12:42:04 +0200

    fuentes neue añadidas

--- blocks/codebaou-hal21-menu/build/index.asset.php
+++ blocks/codebaou-hal21-menu/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'fc4543f1018e6465d2c7');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd380ddd1075b087e508a');
